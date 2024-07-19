551d407a9f93e3476f72b1b07b80b3885728f551
Abrar101102 <abrarahemmad10@gmail.com>
2024-07-19 16:46:38 +0530

    Spelling change of component folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 // eslint-disable-next-line import/extensions
-import Product from "./components/Product";
+import Product from "./Components/Product";
 
 const App = () => <Product />;
 
